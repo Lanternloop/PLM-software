ad492b9b80c6a63d1c63b97f78da9dcdae6990f8
Lanternloop <167588060+Lanternloop@users.noreply.github.com>
2024-11-22 11:45:43 +0100

    Functions database.py
    Inserted function get_connection in database.py for quick database connection in different modules

--- database.py
+++ database.py
@@ -1,25 +1,59 @@
 import sqlite3
-con = sqlite3.connect("fashion_collection")
-cur = con.cursor()
+
+def get_connection():
+    """create fashion collection database and enable foreign key enforcement"""
+    con = sqlite3.connect(":memory:")
+    con.execute("PRAGMA foreign_keys = ON;")
+    return con
 
 
-#creates the table collection
-cur.execute('''
-CREATE TABLE IF NOT EXISTS clothing_collection (
-            collection_id INTEGER PRIMARY KEY NOT NULL, 
-            season_name TEXT NOT NULL, 
-            production_status TEXT NOT NULL
-)
-''')
+def create_tables():
+    """Create fashion collection and product tables"""
+    con = get_connection()
+    cur = con.cursor()
 
-cur.execute('''
-    INSERT INTO clothing_collection VALUES
-        (1, "fall winter 2022", "completed")      
-''')
+    # create table for fashion collection using cur.execute() method
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS collections (
+        collection_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        season_name TEXT NOT NULL,
+        production_status TEXT NOT NULL
+             )""")
 
-con.commit()
+    # create table for products
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS products (
+        product_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        collection_id INTEGER,
+        name TEXT NOT NULL,
+        materials TEXT NOT NULL,
+        fit TEXT NOT NULL,
+        material_certifications TEXT NOT NULL,
+        colors TEXT NOT NULL,
+        processes TEXT,
+        supplier TEXT NOT NULL,
+        gender TEXT NOT NULL, 
+        FOREIGN KEY (collection_id) REFERENCES clothing_collection(collection_id) ON DELETE CASCADE
+                )""")
 
-res = cur.execute("SELECT season_name FROM clothing_collection")
-print(res.fetchall())
 
 
+
+
+    # res = cur.execute("SELECT name FROM sqlite_master")
+    # res.fetchone()
+
+    #commit changes and close connection to database
+    con.commit()
+    con.close()
+
+
+
+
+
+
+
+
+
+# #Refreshes database so that I can run and test code multiple times
+# con = sqlite3.connect(':memory:')
---
+++ inventory.py
@@ -0,0 +1,31 @@
+from database import get_connection
+
+class FashionCollections:
+    """Handles crud operations for collection table and product table"""
+    def __init(self, production = (":memory:")):
+
+
+
+
+
+
+class Garments:
+    """Defines attributes of what products consist of"""
+    def __init__(self, product_id, name, materials, size, fit, material_certifications,
+                 colors, processes, supplier, gender, season):
+        self.product_id = product_id
+        self.name = name
+        self.materials = materials
+        self.size = size
+        self.fit = fit
+        self.materials_certifications = material_certifications
+        self.colors = colors
+        self.processes = processes
+        self.supplier = supplier
+        self.gender = gender
+        self.season = season
+
+
+
+
+
