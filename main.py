6ed142f662dbac0dfa48b24399998e4070e41c8e
Lanternloop <167588060+Lanternloop@users.noreply.github.com>
2024-11-24 18:30:44 +0100

    Functie add_collection
    Heb nieuwe functie toegevoegd die nieuwe collectie toegevoegd aan json file.

--- main.py
+++ main.py
@@ -1,3 +1,52 @@
+from pathlib import Path
+import json
+
+#Maakt connectie met de json file.
+path = Path("database.json")
+
+def create_file():
+    """Kijkt of json file bestaat, anders maakt nieuwe file en importeer lege list."""
+    if path.exists():
+        empty_list = []
+        data = json.dumps(empty_list, indent=4)
+        path.write_text(data)
+
+
+def get_collections():
+    """Toont alle mode collecties met kledingstukken die in de json file zitten."""
+    try:
+        if path.exists():
+            database_json = path.read_text()
+            collections_list = json.loads(database_json)
+            return json.dumps(collections_list, indent=4)
+    except FileNotFoundError:
+        create_file()
+
+
+print(get_collections())
+def add_collection(collection_id, collection_name):
+    """Voegt nieuwe mode collectie aan lijst in json file."""
+    fashion_collections_json = get_collections()
+    fashion_collections_python = json.loads(fashion_collections_json)
+
+    new_collection = {"collections_id": collection_id, #dictionary van nieuwe collectie die wordt toegevoegd.
+                      "collection_name": collection_name,
+                      "styles": []
+    }
+
+    # voegt nieuw collectie aan json file list en schrijft het erin.
+    fashion_collections_python.append(new_collection)
+    path.write_text(json.dumps(fashion_collections_python, indent=4))
+
+    # print bericht dat nieuwe collectie is toegevoegd.
+    print(f"Collection with {collection_id} has been added!")
+    return get_collections()
+
+print(add_collection(2, "Summer 2025"))
+print(add_collection(3, "Summer 2026"))
+
+
+
 
 
 
