cdb61e1869f0858bcaf8fac18869e6a17f40b071
Lanternloop <167588060+Lanternloop@users.noreply.github.com>
2025-01-11 21:09:38 +0100

    get_next_style_id
    Get next style ID functie gemaakt om het creeeren van een nieuwe style id.

--- main_crud_functions.py
+++ main_crud_functions.py
@@ -17,6 +17,21 @@ class ManageStyle:
         self.sizes = sizes
         self.remarks = remarks
 
+    @staticmethod
+    def get_next_id():
+        """Bepaalt de volgende beschikbare style ID"""
+        styles = get_full_collection()
+        last_id = 0
+
+        if styles:
+            # Kijkt wat de volgende beschikbare Style ID is
+            for style in styles:
+                style_id = int(style['Style ID'])
+                if style_id > last_id:
+                    last_id = style_id
+
+        return str(last_id + 1)
+
     def add_style(self):
         """Voegt een nieuwe stijl toe aan het CSV bestand."""
         file_exists = os.path.exists(file_path)  # Controleert of het bestand bestaat
